26bbfd81acd77f3876ad98d62719b4bcd5ab8e6b
Sudipta Banerjee <119493824+sudipta-banerjee03@users.noreply.github.com>
2024-12-04 11:11:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector("nav ul");
+
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+        hamburger.classList.toggle("active");
+    });
+});
+
 const text = "Hi, I'm Sudipta Banerjee";
 const typingEffect = document.getElementById("typing-effect");
 
